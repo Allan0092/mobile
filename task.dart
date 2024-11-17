327fe8865af461414e2d716495c5a3b7f6bcd78c
allan <allan.gtm92@gmail.com>
2024-11-17 22:57:45 +0545

    added fibonacci finder function

--- task.dart
+++ task.dart
@@ -9,9 +9,17 @@ bool is_palindrome(int num){
   return numStr == reversedStr;
 }
 
+int fibonacci(int n) {
+  if (n <= 1) {
+    return n;
+  }
+
+  return fibonacci(n-1)+fibonacci(n-2);
+}
 
 void main(){
 	print('Hello World');
 	print("The sum is "+sum(32, 232).toString());
   print("The number 2332 is palindrome "+ is_palindrome(2332).toString());
+  print("The 10th Fibonacci number is " + fibonacci(10).toString());
 }
