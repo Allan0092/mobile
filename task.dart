2f2025eddb06e8976d6f571759c33671a3346cd1
allan <allan.gtm92@gmail.com>
2024-11-17 22:35:03 +0545

    added is_palindrome function

--- task.dart
+++ task.dart
@@ -1,10 +1,17 @@
 int sum(int num1, int num2){
 	return num1 + num2;
+}
 
+
+bool is_palindrome(int num){
+  String numStr = num.toString();
+  String reversedStr = numStr.split('').reversed.join('');
+  return numStr == reversedStr;
 }
 
 
 void main(){
 	print('Hello World');
-	print(sum(32, 232));
+	print("The sum is "+sum(32, 232).toString());
+  print("The number 2332 is palindrome "+ is_palindrome(2332).toString());
 }
