1243bc479d6a3e73d6576a14701a2484985e2320
allan <allan.gtm92@gmail.com>
2024-11-18 06:10:22 +0545

    added isPrime function

--- task.dart
+++ task.dart
@@ -9,6 +9,7 @@ bool is_palindrome(int num){
   return numStr == reversedStr;
 }
 
+
 int fibonacci(int n) {
   if (n <= 1) {
     return n;
@@ -17,9 +18,24 @@ int fibonacci(int n) {
   return fibonacci(n-1)+fibonacci(n-2);
 }
 
+bool isPrime(int number) {
+  if (number <= 1) {
+    return false;
+  }
+  for (int i=2;i<=number/2;i++) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+
 void main(){
 	print('Hello World');
 	print("The sum is "+sum(32, 232).toString());
   print("The number 2332 is palindrome "+ is_palindrome(2332).toString());
   print("The 10th Fibonacci number is " + fibonacci(10).toString());
+  print("231 is a prime number: "+ isPrime(231).toString());
 }
